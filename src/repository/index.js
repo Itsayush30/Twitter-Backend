2020c36971cacce37a672be191de02a128b7ca62
itsayush30 <itsayush30@gmail.com>
2024-01-11 13:01:08 +0530

    Added comment service to add comments in tweet or comment

--- src/repository/index.js
+++ src/repository/index.js
@@ -2,10 +2,12 @@ import TweetRepository from "./tweet-repository.js";
 import HashtagRepository from "./hashtag-repository.js";
 import LikeRepository from "./like-repository.js";
 import UserRespository from "./user-repository.js";
+import CommentRepository from "./comment-repository.js";
 
-export { 
-    TweetRepository, 
-    HashtagRepository, 
-    UserRespository,
-    LikeRepository,
- };
+export {
+  TweetRepository,
+  HashtagRepository,
+  UserRespository,
+  LikeRepository,
+  CommentRepository,
+};
